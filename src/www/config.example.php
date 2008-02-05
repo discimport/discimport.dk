740e020dc2f1d36633f30973c4170e214b081c8b
Lars Olesen <lars@intraface.dk>
2008-02-05 15:14:01 +0000

    added an example config.

---
+++ src/www/config.example.php
@@ -0,0 +1,10 @@
+<?php
+error_reporting(9999);
+define('INTRAFACEPUBLIC_SHOP_INCLUDE_PATH',
+    '../' . PATH_SEPARATOR .
+    'c:/Users/Lars Olesen/workspace/intrafacepublic/IntrafacePublic/Shop/NewFrontend/src/IntrafacePublic/Shop/templates/' . PATH_SEPARATOR.
+    'c:/Users/Lars Olesen/workspace/intrafacepublic/IntrafacePublic/Shop/NewFrontend/src/' . PATH_SEPARATOR .
+    'c:/Users/Lars Olesen/workspace/intrafacepublic/' . PATH_SEPARATOR . 'c:/Users/Lars Olesen/workspace/konstrukt/lib/' . PATH_SEPARATOR .
+    get_include_path());
+define('INTRAFACE_PRIVATE_KEY', 'privatekeyshouldbereplaced');
+
