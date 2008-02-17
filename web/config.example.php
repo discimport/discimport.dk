e42a7ebc77de48ce6f86d56f0335037778e2147d
Lars Olesen <lars@intraface.dk>
2008-02-17 19:25:39 +0000

    Removed old files.

--- web/config.example.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('PATH_ROOT', 'c:\Documents and Settings\lars\workspace\frisbeebutik\\');
-define('PATH_WWW', 'http://localhost/frisbeebutik/frisbeebutik.dk/');
-define('PATH_CACHE', PATH_ROOT . 'cache/');
-define('INTRAFACE_PRIVATE_KEY', '');
-set_include_path(PATH_INCLUDE . PATH_SEPARATOR . PATH_INCLUDE . '3Party/PEAR/' . PATH_SEPARATOR . get_include_path());
-?>
