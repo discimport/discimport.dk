18ba7014931cd4f48188bd84c68b8a1806ab6582
Lars Olesen <lars@intraface.dk>
2008-07-13 00:21:48 +0000

    Updated example to use.

--- src/www/config.example.php
+++ src/www/config.example.php
@@ -1,10 +1,6 @@
 <?php
 error_reporting(9999);
-define('INTRAFACEPUBLIC_SHOP_INCLUDE_PATH',
-    '../' . PATH_SEPARATOR .
-    'c:/Users/Lars Olesen/workspace/intrafacepublic/IntrafacePublic/Shop/NewFrontend/src/IntrafacePublic/Shop/templates/' . PATH_SEPARATOR.
-    'c:/Users/Lars Olesen/workspace/intrafacepublic/IntrafacePublic/Shop/NewFrontend/src/' . PATH_SEPARATOR .
-    'c:/Users/Lars Olesen/workspace/intrafacepublic/' . PATH_SEPARATOR . 'c:/Users/Lars Olesen/workspace/konstrukt/lib/' . PATH_SEPARATOR .
-    get_include_path());
-define('INTRAFACE_PRIVATE_KEY', 'privatekeyshouldbereplaced');
+define('INTRAFACEPUBLIC_SHOP_INCLUDE_PATH', dirname(__FILE__) . '/../' . PATH_SEPARATOR . get_include_path());
+define('INTRAFACE_PRIVATE_KEY', '');
+define('INTRAFACE_SHOP_ID', 0);
 
