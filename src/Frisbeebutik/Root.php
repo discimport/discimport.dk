3edc470e0d85a8b2fd3dec8d566228a50cf59f2b
Lars Olesen <lars@intraface.dk>
2008-09-18 12:58:20 +0000

    Added getShop() so it can be provided by the context to the shop code.

--- src/Frisbeebutik/Root.php
+++ src/Frisbeebutik/Root.php
@@ -10,7 +10,7 @@ class Frisbeebutik_Root extends k_Dispatcher
         'basket' => 'Indkøbskurv'
     );
 
-    function __construct()
+    public function __construct()
     {
         parent::__construct();
         $this->document->template = dirname(__FILE__) . '/templates/main-tpl.php';
@@ -18,14 +18,19 @@ class Frisbeebutik_Root extends k_Dispatcher
         $this->document->company_name = 'Discimport.dk I/S';        
     }
 
-    function execute()
+    public function execute()
     {
         throw new k_http_Redirect($this->url('shop'));
     }
 
-    function __($phrase)
+    public function __($phrase)
     {
         $translation = new Frisbeebutik_Translation;
         return $translation->get($phrase);
+    }
+    
+    public function getShop()
+    {
+        return $this->registry->get('shop');
     }
 }
