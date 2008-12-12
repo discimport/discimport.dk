9a06cda70d2e77ceadb099f5806d8acb2972dac3
Lars Olesen <lars@intraface.dk>
2008-12-12 18:33:49 +0000

    Added newsletter.

--- src/Frisbeebutik/Root.php
+++ src/Frisbeebutik/Root.php
@@ -15,7 +15,7 @@ class Frisbeebutik_Root extends k_Dispatcher
         parent::__construct();
         $this->document->template = dirname(__FILE__) . '/templates/main-tpl.php';
         $this->document->title = 'Discimport.dk';
-        $this->document->company_name = 'Discimport.dk I/S';        
+        $this->document->company_name = 'Discimport.dk I/S';
     }
 
     public function execute()
@@ -28,9 +28,14 @@ class Frisbeebutik_Root extends k_Dispatcher
         $translation = new Frisbeebutik_Translation;
         return $translation->get($phrase);
     }
-    
+
     public function getShop()
     {
         return $this->registry->get('shop');
+    }
+
+    public function getNewsletter()
+    {
+        return $this->registry->get('newsletter');
     }
 }
