2cc10177def5c4d7fe404c0dfcb108661d615bb9
Lars Olesen <lars@intraface.dk>
2010-07-24 13:53:27 +0000

    Does not report E_DEPRECATED.

--- src/www/config.example.php
+++ src/www/config.example.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting(E_ALL &~E_DEPRECATED);
+
 $GLOBALS['path_include'] =
     '../' . PATH_SEPARATOR .
     get_include_path();
@@ -7,6 +9,7 @@ $GLOBALS['intraface_shop_id'] = 0;
 $GLOBALS["onlinepayment_merchant"] = '';
 $GLOBALS["onlinepayment_md5secret"] = '';
 $GLOBALS["error_log"] = realpath('../errorlog/error.log');
+$GLOBALS["cache_dir"] = realpath(dirname(__FILE__));
 
 set_include_path($GLOBALS['path_include']);
 
